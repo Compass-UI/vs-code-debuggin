f0d5098e11e157a483f969f1fcbcc9d162e99e68
David Shams <david.shams@gmail.com>
2018-07-22 20:08:26 -0500

    ko used in jQuery callback

--- jqscript.js
+++ jqscript.js
@@ -1,3 +1,8 @@
 $(function(){
-    alert($)
+    var vm = {
+        fname: ko.observable('David'),
+        lname: ko.observable('Shams'),
+        phone: ko.observable(4163334444),
+    }
+    ko.applyBindings(vm)
 })
