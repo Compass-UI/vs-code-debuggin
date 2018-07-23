eb09f4d16ac607f4aa900e4cc99e5fb2bfc7de04
David Shams <david.shams@gmail.com>
2018-07-22 20:10:17 -0500

    vm of ko is NOT on global space  - good

--- jqscript.js
+++ jqscript.js
@@ -1,5 +1,6 @@
 $(function(){
-    var vm = {
+    var vm = { /** Note that vm is NOT on global space
+        neither are fname, lname and phone */
         fname: ko.observable('David'),
         lname: ko.observable('Shams'),
         phone: ko.observable(4163334444),
