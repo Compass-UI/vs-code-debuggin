dc81b748126033d678d1bb6fd179851e0829c648
David Shams <david.shams@gmail.com>
2018-08-01 03:17:38 -0500

    TypeScript class that use another TS class

--- jqscript-ts.js
+++ jqscript-ts.js
@@ -1,11 +1,13 @@
-/// <reference path="./node_modules/jquery/dist/jquery.js" />
 /** <reference path="path/to/jquery.d.ts" /> */
 "use strict";
+var simple_mport_1 = require('./simple-mport');
 var UsejQuery = (function () {
     function UsejQuery(hello) {
+        console.log('constructor: ');
         console.log(hello.show());
     }
     return UsejQuery;
 }());
+var uq = new UsejQuery(new simple_mport_1["default"]());
 exports.__esModule = true;
 exports["default"] = UsejQuery;
--- jqscript-ts.ts
+++ jqscript-ts.ts
@@ -1,12 +1,15 @@
-/// <reference path="./node_modules/jquery/dist/jquery.js" />
 /** <reference path="path/to/jquery.d.ts" /> */
 
 import HelloWorld from './simple-mport'
 
 class UsejQuery {
     constructor(hello: HelloWorld){
+        console.log('constructor: ')
         console.log(hello.show())
     }
 }
+
+
+let uq = new UsejQuery(new HelloWorld());
 
 export default UsejQuery;
